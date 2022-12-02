69801fcff03550210cef6ab5b0e0d69d0b77e35d
admin <admin@protonmail.com>
2022-12-02 12:26:59 -0500

    add checkmark

--- app/assets/javascripts/discourse/app/components/wallet-authentication-button.js
+++ app/assets/javascripts/discourse/app/components/wallet-authentication-button.js
@@ -2,7 +2,6 @@ import Component from "@ember/component";
 import { action } from "@ember/object";
 import loadScript from "discourse/lib/load-script";
 import Group from "discourse/models/group";
-import discourseComputed from "discourse-common/utils/decorators";
 
 const UNLOCK_URL = "https://unlock.radiant.capital/unlock.latest.min.js";
 
@@ -12,20 +11,17 @@ export default Component.extend({
   btnClass: "btn-default",
 
   init() {
-    let returnValue = false;
     Group.findAll().then((groups) => {
       const _availableGroups = groups.filterBy("automatic", false);
       _availableGroups.forEach((group) => {
         if (group.name === "rfp-author" || group.name === "rfp-commenter") {
           // eslint-disable-next-line no-console
           console.log("_available groups", _availableGroups);
-          returnValue = true;
           this._isWalletAuthenticated = true;
         }
       });
     });
-
-  }
+  },
 
   @action
   onAuthenticate() {
