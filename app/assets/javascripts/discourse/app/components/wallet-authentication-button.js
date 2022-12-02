924028f4e4202ad229147d6c56f5421fa830af15
admin <admin@protonmail.com>
2022-12-02 08:09:57 -0500

    update button position

---
+++ app/assets/javascripts/discourse/app/components/wallet-authentication-button.js
@@ -0,0 +1,36 @@
+import Component from "@ember/component";
+import { action } from "@ember/object";
+import loadScript from "discourse/lib/load-script";
+import Group from "discourse/models/group";
+import discourseComputed from "discourse-common/utils/decorators";
+
+const UNLOCK_URL = "https://unlock.radiant.capital/unlock.latest.min.js";
+
+export default Component.extend({
+  tagName: "",
+  label: "topic.create",
+  btnClass: "btn-default",
+
+  @action
+  onAuthenticate() {
+    loadScript(UNLOCK_URL).then(() => {
+      window.unlockProtocol.loadCheckoutModal();
+    });
+  },
+
+  @discourseComputed()
+  walletAuthenticated() {
+    let returnValue = false;
+    Group.findAll().then((groups) => {
+        const _availableGroups = groups.filterBy("automatic", false);
+        _availableGroups.map(group => {
+            // eslint-disable-next-line no-console
+            console.log('_available groups', _availableGroups);
+            if (group.name === "rfp-author" || group.name === "rfp-commenter") {
+                returnValue = true;
+            }
+        });
+    });
+    return returnValue;
+  }
+});
